58a708025d81b32d89da85a8f87339bf8ac2cbf1
lord-navy-crypto <haojunli283@gmail.com>
2026-09-04 20:08:30 +0800

    Register analyzer screens for UI Wave II

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -30,6 +30,9 @@ public final class EngineeringUiClientRegistration {
     private static void registerScreens(RegisterMenuScreensEvent event) {
         event.register(EngineeringUiRegistration.SIGNAL_CONDITIONER.get(), SignalConditionerScreen::new);
         event.register(EngineeringUiRegistration.PID_CONTROLLER.get(), PidControllerScreen::new);
+        event.register(EngineeringUiRegistration.OSCILLOSCOPE.get(), OscilloscopeScreen::new);
+        event.register(EngineeringUiRegistration.LOGIC_ANALYZER.get(), LogicAnalyzerScreen::new);
+        event.register(EngineeringUiRegistration.SIGNAL_ANALYZER.get(), SignalAnalyzerScreen::new);
     }
 
     private static void addInventoryDiagnosticsButton(ScreenEvent.Init.Post event) {
