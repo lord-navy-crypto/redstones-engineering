376230a64c966fa0cc9d36a45216369efb874f43
lord-navy-crypto <haojunli283@gmail.com>
2026-09-10 11:09:20 +0800

    Register copper configuration lifecycle regression

--- src/main/java/dev/redstoneengineering/gametest/RseGameTestRegistration.java
+++ src/main/java/dev/redstoneengineering/gametest/RseGameTestRegistration.java
@@ -89,5 +89,6 @@ public final class RseGameTestRegistration {
         event.register(RseDomainNetworkCoverageSystemGameTests.class);
         event.register(RseDomainDriverLifecycleSystemGameTests.class);
         event.register(RseQuartzLabConfigurationLifecycleSystemGameTests.class);
+        event.register(RseCopperConfigurationLifecycleSystemGameTests.class);
     }
 }
