6220d181bda7e7fa8e6d10f3821f4eee7a0530b4
lord-navy-crypto <haojunli283@gmail.com>
2026-09-10 13:42:34 +0800

    Use enhanced field-device dashboard

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -33,7 +33,7 @@ public final class EngineeringUiClientRegistration {
         event.register(EngineeringUiRegistration.OSCILLOSCOPE.get(), OscilloscopeScreen::new);
         event.register(EngineeringUiRegistration.LOGIC_ANALYZER.get(), LogicAnalyzerScreen::new);
         event.register(EngineeringUiRegistration.SIGNAL_ANALYZER.get(), SignalAnalyzerScreen::new);
-        event.register(EngineeringUiRegistration.FIELD_DEVICE.get(), FieldDeviceScreen::new);
+        event.register(EngineeringUiRegistration.FIELD_DEVICE.get(), EnhancedFieldDeviceScreen::new);
         event.register(EngineeringUiRegistration.OPERATIONS_MONITOR.get(), OperationsMonitorScreen::new);
     }
 
