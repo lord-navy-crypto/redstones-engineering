fbbf2c4071fb87541c7b4530928af1d3cf7b82c5
lord-navy-crypto <haojunli283@gmail.com>
2026-09-11 12:43:58 +0800

    Register radio link family screen

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -41,6 +41,7 @@ public final class EngineeringUiClientRegistration {
         event.register(EngineeringUiRegistration.RANGE_SENSOR.get(), RangeSensorScreen::new);
         event.register(EngineeringUiRegistration.SIGNAL_PROCESSOR.get(), SignalProcessorScreen::new);
         event.register(EngineeringUiRegistration.QUARTZ_TIMING.get(), QuartzTimingScreen::new);
+        event.register(EngineeringUiRegistration.RADIO_LINK.get(), RadioLinkScreen::new);
         event.register(EngineeringUiRegistration.OPERATIONS_MONITOR.get(), OperationsMonitorScreen::new);
     }
 
