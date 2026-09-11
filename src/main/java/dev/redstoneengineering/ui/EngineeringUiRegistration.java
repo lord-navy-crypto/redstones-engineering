da068d665cd48e828490b7d926bce28052312f10
lord-navy-crypto <haojunli283@gmail.com>
2026-09-11 12:51:49 +0800

    Register optical system family menu

--- src/main/java/dev/redstoneengineering/ui/EngineeringUiRegistration.java
+++ src/main/java/dev/redstoneengineering/ui/EngineeringUiRegistration.java
@@ -5,6 +5,7 @@ import dev.redstoneengineering.ui.menu.DigitalCommunicationMenu;
 import dev.redstoneengineering.ui.menu.FieldDeviceMenu;
 import dev.redstoneengineering.ui.menu.LogicAnalyzerMenu;
 import dev.redstoneengineering.ui.menu.OperationsMonitorMenu;
+import dev.redstoneengineering.ui.menu.OpticalSystemMenu;
 import dev.redstoneengineering.ui.menu.OscilloscopeMenu;
 import dev.redstoneengineering.ui.menu.PidControllerMenu;
 import dev.redstoneengineering.ui.menu.PneumaticSystemMenu;
@@ -25,58 +26,26 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 
 /**
  * Common-side menu registry for the RSE Engineering Interface System.
- *
- * Menus transport bounded configuration intent and read-only snapshots. They do not own
- * simulation, topology, sampling cadence, controller state, or engineering physics.
+ * Menus transport bounded configuration intent and read-only snapshots; simulation remains server-owned.
  */
 @Mod(RedstoneEngineering.MOD_ID)
 public final class EngineeringUiRegistration {
-    public static final DeferredRegister<MenuType<?>> MENUS =
-            DeferredRegister.create(Registries.MENU, RedstoneEngineering.MOD_ID);
+    public static final DeferredRegister<MenuType<?>> MENUS = DeferredRegister.create(Registries.MENU, RedstoneEngineering.MOD_ID);
+    public static final DeferredHolder<MenuType<?>, MenuType<SignalConditionerMenu>> SIGNAL_CONDITIONER = MENUS.register("signal_conditioner", () -> IMenuTypeExtension.create(SignalConditionerMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<PidControllerMenu>> PID_CONTROLLER = MENUS.register("pid_controller", () -> IMenuTypeExtension.create(PidControllerMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<OscilloscopeMenu>> OSCILLOSCOPE = MENUS.register("oscilloscope", () -> IMenuTypeExtension.create(OscilloscopeMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<LogicAnalyzerMenu>> LOGIC_ANALYZER = MENUS.register("logic_analyzer", () -> IMenuTypeExtension.create(LogicAnalyzerMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<SignalAnalyzerMenu>> SIGNAL_ANALYZER = MENUS.register("signal_analyzer", () -> IMenuTypeExtension.create(SignalAnalyzerMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<FieldDeviceMenu>> FIELD_DEVICE = MENUS.register("field_device", () -> IMenuTypeExtension.create(FieldDeviceMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<UniversalFieldDeviceMenu>> UNIVERSAL_FIELD_DEVICE = MENUS.register("universal_field_device", () -> IMenuTypeExtension.create(UniversalFieldDeviceMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<RangeSensorMenu>> RANGE_SENSOR = MENUS.register("range_sensor", () -> IMenuTypeExtension.create(RangeSensorMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<SignalProcessorMenu>> SIGNAL_PROCESSOR = MENUS.register("signal_processor", () -> IMenuTypeExtension.create(SignalProcessorMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<QuartzTimingMenu>> QUARTZ_TIMING = MENUS.register("quartz_timing", () -> IMenuTypeExtension.create(QuartzTimingMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<RadioLinkMenu>> RADIO_LINK = MENUS.register("radio_link", () -> IMenuTypeExtension.create(RadioLinkMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<DigitalCommunicationMenu>> DIGITAL_COMMUNICATION = MENUS.register("digital_communication", () -> IMenuTypeExtension.create(DigitalCommunicationMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<PneumaticSystemMenu>> PNEUMATIC_SYSTEM = MENUS.register("pneumatic_system", () -> IMenuTypeExtension.create(PneumaticSystemMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<OpticalSystemMenu>> OPTICAL_SYSTEM = MENUS.register("optical_system", () -> IMenuTypeExtension.create(OpticalSystemMenu::new));
+    public static final DeferredHolder<MenuType<?>, MenuType<OperationsMonitorMenu>> OPERATIONS_MONITOR = MENUS.register("operations_monitor", () -> IMenuTypeExtension.create(OperationsMonitorMenu::new));
 
-    public static final DeferredHolder<MenuType<?>, MenuType<SignalConditionerMenu>> SIGNAL_CONDITIONER =
-            MENUS.register("signal_conditioner", () -> IMenuTypeExtension.create(SignalConditionerMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<PidControllerMenu>> PID_CONTROLLER =
-            MENUS.register("pid_controller", () -> IMenuTypeExtension.create(PidControllerMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<OscilloscopeMenu>> OSCILLOSCOPE =
-            MENUS.register("oscilloscope", () -> IMenuTypeExtension.create(OscilloscopeMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<LogicAnalyzerMenu>> LOGIC_ANALYZER =
-            MENUS.register("logic_analyzer", () -> IMenuTypeExtension.create(LogicAnalyzerMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<SignalAnalyzerMenu>> SIGNAL_ANALYZER =
-            MENUS.register("signal_analyzer", () -> IMenuTypeExtension.create(SignalAnalyzerMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<FieldDeviceMenu>> FIELD_DEVICE =
-            MENUS.register("field_device", () -> IMenuTypeExtension.create(FieldDeviceMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<UniversalFieldDeviceMenu>> UNIVERSAL_FIELD_DEVICE =
-            MENUS.register("universal_field_device", () -> IMenuTypeExtension.create(UniversalFieldDeviceMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<RangeSensorMenu>> RANGE_SENSOR =
-            MENUS.register("range_sensor", () -> IMenuTypeExtension.create(RangeSensorMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<SignalProcessorMenu>> SIGNAL_PROCESSOR =
-            MENUS.register("signal_processor", () -> IMenuTypeExtension.create(SignalProcessorMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<QuartzTimingMenu>> QUARTZ_TIMING =
-            MENUS.register("quartz_timing", () -> IMenuTypeExtension.create(QuartzTimingMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<RadioLinkMenu>> RADIO_LINK =
-            MENUS.register("radio_link", () -> IMenuTypeExtension.create(RadioLinkMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<DigitalCommunicationMenu>> DIGITAL_COMMUNICATION =
-            MENUS.register("digital_communication", () -> IMenuTypeExtension.create(DigitalCommunicationMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<PneumaticSystemMenu>> PNEUMATIC_SYSTEM =
-            MENUS.register("pneumatic_system", () -> IMenuTypeExtension.create(PneumaticSystemMenu::new));
-
-    public static final DeferredHolder<MenuType<?>, MenuType<OperationsMonitorMenu>> OPERATIONS_MONITOR =
-            MENUS.register("operations_monitor", () -> IMenuTypeExtension.create(OperationsMonitorMenu::new));
-
-    public EngineeringUiRegistration(IEventBus modBus) {
-        MENUS.register(modBus);
-    }
+    public EngineeringUiRegistration(IEventBus modBus) { MENUS.register(modBus); }
 }
