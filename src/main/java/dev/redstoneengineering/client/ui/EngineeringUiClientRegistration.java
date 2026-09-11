b5d21e5320b22c2aa5545aac93115e8d4a859b1b
lord-navy-crypto <haojunli283@gmail.com>
2026-09-11 12:46:07 +0800

    Register digital communication family screen

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -42,6 +42,7 @@ public final class EngineeringUiClientRegistration {
         event.register(EngineeringUiRegistration.SIGNAL_PROCESSOR.get(), SignalProcessorScreen::new);
         event.register(EngineeringUiRegistration.QUARTZ_TIMING.get(), QuartzTimingScreen::new);
         event.register(EngineeringUiRegistration.RADIO_LINK.get(), RadioLinkScreen::new);
+        event.register(EngineeringUiRegistration.DIGITAL_COMMUNICATION.get(), DigitalCommunicationScreen::new);
         event.register(EngineeringUiRegistration.OPERATIONS_MONITOR.get(), OperationsMonitorScreen::new);
     }
 
