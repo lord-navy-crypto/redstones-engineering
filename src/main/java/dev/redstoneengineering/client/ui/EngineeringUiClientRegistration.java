7a5d652d165ee366725ce5612c2079e6d4dccb78
lord-navy-crypto <haojunli283@gmail.com>
2026-09-11 12:39:24 +0800

    Register dedicated range sensor screen

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -38,6 +38,7 @@ public final class EngineeringUiClientRegistration {
         event.register(EngineeringUiRegistration.SIGNAL_ANALYZER.get(), SignalAnalyzerScreen::new);
         event.register(EngineeringUiRegistration.FIELD_DEVICE.get(), EnhancedFieldDeviceScreen::new);
         event.register(EngineeringUiRegistration.UNIVERSAL_FIELD_DEVICE.get(), UniversalFieldDeviceScreen::new);
+        event.register(EngineeringUiRegistration.RANGE_SENSOR.get(), RangeSensorScreen::new);
         event.register(EngineeringUiRegistration.OPERATIONS_MONITOR.get(), OperationsMonitorScreen::new);
     }
 
