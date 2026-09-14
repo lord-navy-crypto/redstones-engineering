5af379018d29b14cbaa89e925937e1a799d05390
lord-navy-crypto <haojunli283@gmail.com>
2026-09-14 12:40:02 +0800

    Fix tablet newest snapshot key

--- src/main/java/dev/redstoneengineering/item/DiagnosticTabletItem.java
+++ src/main/java/dev/redstoneengineering/item/DiagnosticTabletItem.java
@@ -116,7 +116,7 @@ public final class DiagnosticTabletItem extends Item {
                 String previous = tag.getString(SNAPSHOT_PREFIX + (i - 1));
                 if (!previous.isBlank()) tag.putString(SNAPSHOT_PREFIX + i, previous);
             }
-            tag.putString(SNAPSHOT_PREFIX, snapshot);
+            tag.putString(SNAPSHOT_PREFIX + 0, snapshot);
             tag.putInt(COUNT, newCount);
         });
     }
