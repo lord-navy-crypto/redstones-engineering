a590883849c81dab60c1812e0596debf8a583880
lord-navy-crypto <haojunli283@gmail.com>
2026-09-15 12:26:51 +0800

    Register Industrial Buffer screen

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -52,6 +52,7 @@ public final class EngineeringUiClientRegistration {
         event.register(EngineeringUiRegistration.RELIABILITY_SYSTEM.get(), ReliabilitySystemScreen::new);
         event.register(EngineeringUiRegistration.OPERATIONS_MONITOR.get(), OperationsMonitorScreen::new);
         event.register(EngineeringUiRegistration.WORKCELL_CONTROLLER.get(), WorkcellControllerScreen::new);
+        event.register(EngineeringUiRegistration.INDUSTRIAL_BUFFER.get(), IndustrialBufferScreen::new);
         event.register(EngineeringUiRegistration.REDSTONE_ENCYCLOPEDIA.get(), RedstoneEncyclopediaScreen::new);
         event.register(EngineeringUiRegistration.DIAGNOSTIC_TABLET.get(), DiagnosticTabletScreen::new);
     }
