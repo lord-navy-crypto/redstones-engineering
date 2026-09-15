4d370e7253e1a3556b122ea28e1a1bf47aafa6bd
lord-navy-crypto <haojunli283@gmail.com>
2026-09-15 20:19:00 +0800

    Register persistent plant runtime GameTests

--- src/main/java/dev/redstoneengineering/gametest/RseGameTestRegistration.java
+++ src/main/java/dev/redstoneengineering/gametest/RseGameTestRegistration.java
@@ -24,6 +24,7 @@ public final class RseGameTestRegistration {
         event.register(RseEngineeringUiGameTests.class);
         event.register(RseOperationsTimelineGameTests.class);
         event.register(RseOperationsIncidentGameTests.class);
+        event.register(RsePersistentPlantRuntimeGameTests.class);
         event.register(RseElectricalReliabilityGameTests.class);
         event.register(RseCommunicationIdentityGameTests.class);
         event.register(RseSignalJunctionTopologyGameTests.class);
