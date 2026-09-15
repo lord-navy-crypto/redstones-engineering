3106704e99ac48a6666f323bb7bf80bb66798264
lord-navy-crypto <haojunli283@gmail.com>
2026-09-15 11:53:17 +0800

    Register Workcell Controller screen

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -51,6 +51,7 @@ public final class EngineeringUiClientRegistration {
         event.register(EngineeringUiRegistration.MAGNETIC_SYSTEM.get(), MagneticSystemScreen::new);
         event.register(EngineeringUiRegistration.RELIABILITY_SYSTEM.get(), ReliabilitySystemScreen::new);
         event.register(EngineeringUiRegistration.OPERATIONS_MONITOR.get(), OperationsMonitorScreen::new);
+        event.register(EngineeringUiRegistration.WORKCELL_CONTROLLER.get(), WorkcellControllerScreen::new);
         event.register(EngineeringUiRegistration.REDSTONE_ENCYCLOPEDIA.get(), RedstoneEncyclopediaScreen::new);
         event.register(EngineeringUiRegistration.DIAGNOSTIC_TABLET.get(), DiagnosticTabletScreen::new);
     }
