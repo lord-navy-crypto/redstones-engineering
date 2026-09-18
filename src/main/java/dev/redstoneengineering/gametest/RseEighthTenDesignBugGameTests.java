1492fd6fa8c6c1e924e32ae9c89602c0cdbefd8a
lord-navy-crypto <haojunli283@gmail.com>
2026-09-19 00:23:40 +0800

    test: cover retained conversion codes across evidence loss

--- src/main/java/dev/redstoneengineering/gametest/RseEighthTenDesignBugGameTests.java
+++ src/main/java/dev/redstoneengineering/gametest/RseEighthTenDesignBugGameTests.java
@@ -2,6 +2,7 @@ package dev.redstoneengineering.gametest;
 
 import dev.redstoneengineering.RedstoneEngineering;
 import dev.redstoneengineering.block.DirectionalDomainBlock;
+import dev.redstoneengineering.block.DirectionalDomainSourceBlock;
 import dev.redstoneengineering.block.DirectionalRedstoneEndpointBlock;
 import dev.redstoneengineering.block.LapisPrecisionSourceBlock;
 import dev.redstoneengineering.block.LapisToRedstoneQuantizerBlock;
@@ -19,6 +20,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.gametest.PrefixGameTestTemplate;
 
@@ -314,4 +316,61 @@ public final class RseEighthTenDesignBugGameTests {
         }
         helper.succeed();
     }
+    @PrefixGameTestTemplate(false)
+    @GameTest(templateNamespace = RedstoneEngineering.MOD_ID, template = TEMPLATE, timeoutTicks = 90)
+    public static void conversionBridgesRetainLastCodeWhenEvidenceDisappears(GameTestHelper helper) {
+        BlockPos redstoneSource = new BlockPos(1, 1, 1);
+        BlockPos scaler = new BlockPos(2, 1, 1);
+        BlockPos lapisSource = new BlockPos(1, 1, 3);
+        BlockPos quantizer = new BlockPos(2, 1, 3);
+
+        helper.setBlock(redstoneSource, RedstoneEngineering.REDSTONE_REFERENCE_SOURCE.get().defaultBlockState()
+                .setValue(DirectionalRedstoneEndpointBlock.FACING, Direction.EAST)
+                .setValue(RedstoneReferenceSourceBlock.POWER, 12));
+        helper.setBlock(scaler, RedstoneEngineering.REDSTONE_TO_LAPIS_SCALER.get().defaultBlockState()
+                .setValue(RedstoneToLapisScalerBlock.FACING, Direction.EAST)
+                .setValue(RedstoneToLapisScalerBlock.INPUT_FACING, Direction.WEST));
+
+        helper.setBlock(lapisSource, RedstoneEngineering.LAPIS_PRECISION_SOURCE.get().defaultBlockState()
+                .setValue(DirectionalDomainSourceBlock.FACING, Direction.EAST)
+                .setValue(LapisPrecisionSourceBlock.VALUE, 80));
+        helper.setBlock(quantizer, RedstoneEngineering.LAPIS_TO_REDSTONE_QUANTIZER.get().defaultBlockState()
+                .setValue(LapisToRedstoneQuantizerBlock.FACING, Direction.EAST)
+                .setValue(LapisToRedstoneQuantizerBlock.INPUT_FACING, Direction.WEST));
+
+        helper.runAfterDelay(6, () -> {
+            BlockPos scalerWorld = helper.absolutePos(scaler);
+            BlockPos quantizerWorld = helper.absolutePos(quantizer);
+            int scaledBefore = RedstoneToLapisScalerBlock.outputValue(helper.getLevel(), scalerWorld);
+            int quantizedBefore = helper.getBlockState(quantizer).getValue(LapisToRedstoneQuantizerBlock.POWER);
+
+            if (scaledBefore <= 0
+                    || RedstoneToLapisScalerBlock.outputQuality(helper.getLevel(), scalerWorld) != PortQuality.VALID
+                    || quantizedBefore <= 0
+                    || LapisToRedstoneQuantizerBlock.outputQuality(helper.getLevel(), quantizerWorld) != PortQuality.VALID) {
+                helper.fail("Conversion bridges did not establish trustworthy non-zero baselines", scaler);
+                return;
+            }
+
+            helper.setBlock(redstoneSource, Blocks.AIR.defaultBlockState());
+            helper.setBlock(lapisSource, Blocks.AIR.defaultBlockState());
+
+            helper.runAfterDelay(6, () -> {
+                int scaledAfter = RedstoneToLapisScalerBlock.outputValue(helper.getLevel(), scalerWorld);
+                int quantizedAfter = helper.getBlockState(quantizer).getValue(LapisToRedstoneQuantizerBlock.POWER);
+                PortQuality scalerQuality = RedstoneToLapisScalerBlock.outputQuality(helper.getLevel(), scalerWorld);
+                PortQuality quantizerQuality = LapisToRedstoneQuantizerBlock.outputQuality(helper.getLevel(), quantizerWorld);
+
+                if (scaledAfter != scaledBefore
+                        || quantizedAfter != quantizedBefore
+                        || scalerQuality == PortQuality.VALID
+                        || quantizerQuality == PortQuality.VALID) {
+                    helper.fail("Missing upstream evidence was converted into a new numerical code", scaler);
+                    return;
+                }
+                helper.succeed();
+            });
+        });
+    }
+
 }
