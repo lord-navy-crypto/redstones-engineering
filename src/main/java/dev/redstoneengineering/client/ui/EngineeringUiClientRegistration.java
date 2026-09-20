b49f8d9c366435d09c28b0088bf0ba37a0890ed4
lord-navy-crypto <haojunli283@gmail.com>
2026-09-20 22:30:37 +0800

    Register ten-block engineering parameter notebook screen

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -44,6 +44,7 @@ public final class EngineeringUiClientRegistration {
         event.register(EngineeringUiRegistration.SIGNAL_PROCESSOR.get(), SignalProcessorScreen::new);
         event.register(EngineeringUiRegistration.LAPIS_LOW_PASS_FILTER.get(), LapisLowPassFilterScreen::new);
         event.register(EngineeringUiRegistration.SERVO_ACTUATOR.get(), ServoActuatorNotebookScreen::new);
+        event.register(EngineeringUiRegistration.MULTI_PHYSICS_PARAMETER.get(), MultiPhysicsParameterNotebookScreen::new);
         event.register(EngineeringUiRegistration.QUARTZ_TIMING.get(), QuartzTimingScreen::new);
         event.register(EngineeringUiRegistration.RADIO_LINK.get(), RadioLinkScreen::new);
         event.register(EngineeringUiRegistration.DIGITAL_COMMUNICATION.get(), DigitalCommunicationScreen::new);
