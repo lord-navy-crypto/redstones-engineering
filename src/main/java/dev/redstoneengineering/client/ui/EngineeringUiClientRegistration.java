a83fc40c79999c1977756afe99e3810b4cb7ac8c
lord-navy-crypto <haojunli283@gmail.com>
2026-09-20 21:50:19 +0800

    Register Lapis filter engineering notebook screen

--- src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
+++ src/main/java/dev/redstoneengineering/client/ui/EngineeringUiClientRegistration.java
@@ -42,6 +42,7 @@ public final class EngineeringUiClientRegistration {
         event.register(EngineeringUiRegistration.MEDIA_CONVERSION.get(), MediaConversionScreen::new);
         event.register(EngineeringUiRegistration.RANGE_SENSOR.get(), RangeSensorScreen::new);
         event.register(EngineeringUiRegistration.SIGNAL_PROCESSOR.get(), SignalProcessorScreen::new);
+        event.register(EngineeringUiRegistration.LAPIS_LOW_PASS_FILTER.get(), LapisLowPassFilterScreen::new);
         event.register(EngineeringUiRegistration.QUARTZ_TIMING.get(), QuartzTimingScreen::new);
         event.register(EngineeringUiRegistration.RADIO_LINK.get(), RadioLinkScreen::new);
         event.register(EngineeringUiRegistration.DIGITAL_COMMUNICATION.get(), DigitalCommunicationScreen::new);
